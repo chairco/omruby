e4a25732d28a218a24753481728535d02fa3ed0a
chairco <chairco@gmail.com>
2018-02-06 15:24:01 +0800

    Change index link with customize and fix syntax on rails's Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+    get "signin", to: 'devise/sessions#new'
+    delete "signout", to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
+  end
+  
   root 'pages#home'
-  #get 'pages/about'
   get 'about' => 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
